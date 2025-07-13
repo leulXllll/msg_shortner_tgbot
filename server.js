8193e1e5fc375724ead233cfdbd95043463c3a69
Leul <leulzewduchanie@gmail.com>
2025-07-13 14:11:21 +0300

    cleaned up commented and unused code

--- server.js
+++ server.js
@@ -5,15 +5,6 @@ const token = process.env.TELEGRAM_BOT;
 
 const bot = new TelegramBot(token, {polling: true});
 
-// const expiredOn = /^expired on .*\.$/
-let expiredOn = /expired on (.*?)\./;
-
-
-// "Dear Customer, your remaining amount  from Gift Monthly Internet Package 50GB from telebirr to be expired after 30 days is 35039.141 MB with expiry date on 2025-07-27 at 13:33:56;  from Monthly SIM Starter_115Min Voice Package Plus 1GB data Bonus Prorated with Three Month_20 ETB is 115 minute and 0 second with expiry date on 2025-07-16 at 13:00:06;    Enjoy 10% additional rewards by downloading telebirr SuperApp http://onelink.to/fpgu4m. Thank you! Ethio telecom."
-
-// "Dear Customer, your remaining amount from Daily voice 14 Minutes and 3 SMS with 14 Minutes night package bonus to be expired after 24 hours is 14 minute and 0 second with expiry date on 2025-02-22 at 17:00:51;     from Daily voice 14 Minutes and 3 SMS with 14 Minutes night package bonus to be expired after 24 hours is 3 SMS with expiry date on 2025-02-22 at 17:00:51; from Daily voice 14 Minutes and 3 SMS with 14 Minutes night package bonus to be expired after 24 hours is 13 minute and 56 second with expiry date on 2025-02-22 at 17:00:51;    Enjoy 10% additional rewards by downloading telebirr SuperApp http://onelink.to/fpgu4m.Thank you!Ethio telecom."
-
-
 
 bot.onText(/\/start/,(msg)=>{
   bot.sendMessage(msg.chat.id,"Welcome this is Tele Message shortner Bot Copy and Paste your tele Response")
